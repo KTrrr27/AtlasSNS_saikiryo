87128caa6aab7a7f2f581e624f2b3611afc34d03
ryo27ttyo <ryo27.ttyo@gmail.com>
2025-07-25 23:34:06 +0900

    リスト作成　アイコン　ポスト

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,14 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Post;
+
 
 class FollowsController extends Controller
 {
     //
-    public function followList(){
-        return view('follows.followList');
+    public function followList()
+    {
+        $users = User::get();
+
+        //認証情報のフォローIDを準備
+        $user = auth()->user();
+        //認証済みのIDがフォローしているID取得
+        $followingIDs = $user->followings;
+        // postの情報を降順に取得
+        $posts = Post::with('user')
+            ->whereIn('user_id', $followingIDs->pluck('id'))
+            ->orderby('created_at', 'desc')
+            ->get();
+        return view('follows.followList', ['followingIDs' => $followingIDs, 'posts' => $posts]);
     }
-    public function followerList(){
-        return view('follows.followerList');
+    public function followerList()
+    {
+        $users = User::get();
+
+        //認証情報のフォローIDを準備
+        $user = auth()->user();
+        //認証済みのIDがフォローしているID取得
+        $followerIDs = $user->followers;
+        // postの情報を降順に取得
+        $posts = Post::with('user')
+            ->whereIn('user_id', $followerIDs->pluck('id'))
+            ->orderby('created_at', 'desc')
+            ->get();
+        return view('follows.followerList', ['followerIDs' => $followerIDs, 'posts' => $posts]);
     }
 }
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,6 +1,30 @@
 <x-login-layout>
-
-  <h1>followerList画面</h1>
-  <h2>機能を実装していきましょう。</h2>
-
+  <div class="follow-list area">
+    <h1 class="list-title">フォローリスト</h1>
+    <ul>
+      @foreach($followerIDs as $user)
+      @continue($user->id==auth()->id())
+      <a href="">
+        <li><img src="{{ asset('images/' . $user->icon_image) }}" alt="ユーザーアイコン" class="list_icon"></li>
+      </a>
+      @endforeach
+    </ul>
+  </div>
+  @foreach ($posts as $post)
+  <ul class="post_ul">
+    <li class="post_left">
+      <a href="">
+        <img src="{{ asset('images/' .$post->user->icon_image) }}" alt="ユーザーアイコン" class="user_icon">
+      </a>
+      <div>
+        <p>{{ $post->user->username }}</p>
+        <!-- 改行 !! nl2br !! -->
+        <p>{!! nl2br( $post->post ) !!}</p>
+      </div>
+    </li>
+    <li class="post_right">
+      <p>{{ $post->created_at }}</p>
+    </li>
+  </ul>
+  @endforeach
 </x-login-layout>
